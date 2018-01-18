d574215f752e5e8079d4aa859c2ca918fd62609d
Jarkko Saikkonen <jarkko.saikkonen@gmail.com>
2018-01-18 15:01:42 +0200

    VirtualHarri files

---
+++ MainActivity.java
@@ -0,0 +1,59 @@
+package fi.metropolia.virtualharri;
+
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+import android.os.PowerManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // This needs to happen when the broadcast is received (when the new activity launches)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+    }
+}
+
+class PowerConnectionReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Intent microsoftTeamsIntent = context.getPackageManager().getLaunchIntentForPackage("com" + ".microsoft.teams");
+
+        /*if(action.equals(Intent.ACTION_USER_PRESENT)){
+            Toast.makeText(context, "User is present", Toast.LENGTH_SHORT).show();
+        }*/
+
+        if (action.equals(Intent.ACTION_POWER_CONNECTED)) {
+            Log.e("Charger State", "power connected");
+
+        } else if (action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
+            Log.e("Charger State", "power disconnected");
+
+            if (microsoftTeamsIntent != null) {
+
+                microsoftTeamsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(microsoftTeamsIntent);
+            } else {
+                Toast.makeText(context, "Microsoft Teams couldn't be launched", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
